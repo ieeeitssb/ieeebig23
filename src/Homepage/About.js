add8d6b6a16acb07267dd9de5e7dfc31700ddf7d
reynaldineo <reynaldineoramadhani@gmail.com>
2023-07-26 20:50:05 +0700

    style: update about title & logo

--- src/Homepage/About.js
+++ src/Homepage/About.js
@@ -6,16 +6,16 @@ export default function About() {
     <section className="w-full min-h-[600px] bg-light dark:bg-dark bg-cover p-10">
       <div className="container">
         <h1 className="text-3xl font-bold text-black dark:text-white text-center mt-10 uppercase tracking-wide mb-3">
-          About
+          About IEEE BIG
         </h1>
-        <hr className="border-t-4 w-[250px] mx-auto border-t-teal-600 dark:border-t-white" />
+        <hr className="border-t-4 w-[250px] md:w-[300px] mx-auto border-t-teal-600 dark:border-t-white" />
         <div className="container">
           <div className="mt-10 flex flex-wrap justify-center items-center">
-            <div className="w-full relative flex md:w-1/3">
+            <div className="w-full relative flex pb-8 md:w-1/3">
               <img
                 src={logo}
                 alt="gambar"
-                className="mx-auto w-56 rounded-full border-2 shadow-lg"
+                className="mx-auto w-36 md:w-56 rounded-full border-2 shadow-lg"
               />
             </div>
             <div className="w-full mb-10 md:w-1/2">
