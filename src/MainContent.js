66d5aea961e5005d23a54eb1de4d46530131464d
reynaldineo <reynaldineoramadhani@gmail.com>
2023-07-29 21:05:43 +0700

    fix: update events route (IIP, BCC, CF)

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,13 +2,17 @@ import React from "react";
 import MainHome from "./Homepage/MainHome";
 import MainBCC from "./BCC/MainBCC";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainIIP from "./IIP/MainIIP";
+import MainCF from "./CompanyFair/MainCF";
 export default function MainContent() {
   return (
     <section className="w-full min-h-screen">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainHome />} />
-          <Route path="/event/bcc" element={<MainBCC />} />
+          <Route path="/events/iip" element={<MainIIP />} />
+          <Route path="/events/bcc" element={<MainBCC />} />
+          <Route path="/events/companyfair" element={<MainCF />} />
         </Routes>
       </BrowserRouter>
     </section>
