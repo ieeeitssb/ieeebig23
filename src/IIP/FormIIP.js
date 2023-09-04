527e8a86b332fb60742e71e64f1eee5976d091cd
rmdhnz <ramadhanfadhly2@gmail.com>
2023-09-04 16:52:01 +0700

    submit form to googlesheet

--- src/IIP/FormIIP.js
+++ src/IIP/FormIIP.js
@@ -10,8 +10,34 @@ export default function FormIIP() {
       <div className="container mt-3 selection:bg-blue-600 selection:text-white">
         <form
           action=""
+          id="iip-form"
           onSubmit={(e) => {
             e.preventDefault();
+            const btnSubmit = document.querySelector(".btn-submit"),
+              btnLoading = document.querySelector(".btn-loading"),
+              alert = document.querySelector(".alert");
+            btnLoading.classList.toggle("hidden");
+            btnSubmit.classList.toggle("hidden");
+            fetch(
+              "https://script.google.com/macros/s/AKfycbwLVthoUJz_9i3rCHOWc76GHhGt-lySINUyJqQkhsZeT3CY7tG07Q9amiCpvFTb-Usp/exec",
+              {
+                method: "POST",
+                body: new FormData(document.getElementById("iip-form")),
+              }
+            )
+              .then((response) => {
+                console.log("Success! ", response);
+                document.getElementById("iip-form").reset();
+                btnLoading.classList.toggle("hidden");
+                btnSubmit.classList.toggle("hidden");
+                alert.classList.toggle("hidden");
+                setTimeout(() => {
+                  alert.classList.toggle("hidden");
+                }, 3000);
+              })
+              .catch((err) => {
+                console.log("Error : ", err.message);
+              });
           }}
         >
           <div className="mb-3 row justify-center">
@@ -19,7 +45,12 @@ export default function FormIIP() {
               <label htmlFor="" className="form-label mylabel">
                 Team Name
               </label>
-              <input type="text" className="form-control myinput" required />
+              <input
+                type="text"
+                name="team"
+                className="form-control myinput"
+                required
+              />
             </div>
             <div className="col-md-6">
               <label
@@ -30,6 +61,7 @@ export default function FormIIP() {
               </label>
               <input
                 type="text"
+                name="institution"
                 required
                 className="form-control bg-slate-300 border-teal-500 border-2 focus:bg-slate-100"
               />
@@ -48,19 +80,34 @@ export default function FormIIP() {
                 <label htmlFor="" className="form-label mylabel">
                   Full Name
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="leader"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
                   Major
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="major-leader"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
                   Student ID
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="id-leader"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
@@ -69,6 +116,7 @@ export default function FormIIP() {
                 <input
                   required
                   type="email"
+                  name="email-leader"
                   placeholder="example@gmail.com"
                   className="form-control myinput"
                 />
@@ -80,6 +128,7 @@ export default function FormIIP() {
                 <input
                   required
                   type="text"
+                  name="contact-leader"
                   placeholder="example@gmail.com"
                   className="form-control myinput"
                 />
@@ -97,19 +146,34 @@ export default function FormIIP() {
                 <label htmlFor="" className="form-label mylabel">
                   Full Name
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="member1-name"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
                   Major
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="member1-major"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
                   Student ID
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="member1-id"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
@@ -118,6 +182,7 @@ export default function FormIIP() {
                 <input
                   required
                   type="email"
+                  name="member1-email"
                   placeholder="example@gmail.com"
                   className="form-control myinput"
                 />
@@ -128,6 +193,7 @@ export default function FormIIP() {
                 </label>
                 <input
                   type="text"
+                  name="member1-contact"
                   placeholder="Optional"
                   className="form-control myinput"
                 />
@@ -147,19 +213,34 @@ export default function FormIIP() {
                 <label htmlFor="" className="form-label mylabel">
                   Full Name
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="member2-name"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
                   Major
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="member2-major"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
                   Student ID
                 </label>
-                <input required type="text" className="form-control myinput" />
+                <input
+                  required
+                  type="text"
+                  name="member2-id"
+                  className="form-control myinput"
+                />
               </div>
               <div className="row mb-3">
                 <label htmlFor="" className="form-label mylabel">
@@ -170,6 +251,7 @@ export default function FormIIP() {
                   type="email"
                   placeholder="example@gmail.com"
                   className="form-control myinput"
+                  name="member2-email"
                 />
               </div>
               <div className="row mb-3">
@@ -179,6 +261,7 @@ export default function FormIIP() {
                 <input
                   type="text"
                   placeholder="Optional"
+                  name="member2-contact"
                   className="form-control myinput"
                 />
               </div>
@@ -187,7 +270,7 @@ export default function FormIIP() {
               <div className="row mb-3">
                 <div className="col">
                   <h1 className="dark:text-white font-bold text-3xl text-center">
-                    Team Member 2 <sup>nd</sup>
+                    Link of requirements
                   </h1>
                 </div>
               </div>
@@ -197,23 +280,51 @@ export default function FormIIP() {
                     Link of your drive (Student ID Card, Follow IG IEEEBIG
                     Proofment, Twibbon IG Proofment.)
                   </label>
-                  <input required type="url" className="form-control myinput" />
+                  <input
+                    required
+                    type="url"
+                    name="requirement"
+                    className="form-control myinput"
+                  />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="" className="form-label mylabel">
                     Link of your drive (Upload your Abstract/Executive
                     Summary..)
                   </label>
-                  <input required type="url" className="form-control myinput" />
+                  <input
+                    required
+                    type="url"
+                    name="summary"
+                    className="form-control myinput"
+                  />
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <div className="mb-5 p-3 bg-teal-400 text-white w-full md:w-3/4 mx-auto lg:w-1/2 rounded-lg hidden alert">
+                Thank You for submitting! Stay tune with our social media
               </div>
             </div>
           </div>
           <button
             type="submit"
-            className="px-4 py-2 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-800 focus:ring-2 focus:ring-blue-400"
+            className="px-4 py-2 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-800 focus:ring-2 focus:ring-blue-400 btn-submit"
           >
             Submit
+          </button>
+          <button
+            class="btn btn-primary btn-loading hidden"
+            type="button"
+            disabled
+          >
+            <span
+              class="spinner-border spinner-border-sm"
+              aria-hidden="true"
+            ></span>
+            <span role="status">Loading...</span>
           </button>
         </form>
       </div>
