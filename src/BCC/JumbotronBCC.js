57306b1684a4b6d5017cbb1965307cd99628357c
rmdhnz <ramadhanfadhly2@gmail.com>
2023-11-09 18:44:42 +0700

    mengubah link guidebook

--- src/BCC/JumbotronBCC.js
+++ src/BCC/JumbotronBCC.js
@@ -12,7 +12,7 @@ export default function JumbotronBCC() {
         </p>
         <a
           className="py-2 px-4 rounded-md bg-blue-600 text-white hover:bg-blue-800 mt-5 font-oswald"
-          href="https://drive.google.com/file/d/1lIdEplM1Wu45IsxmqKXWMPcVq71x7GXc/view?usp=drivesdk/"
+          href="https://drive.google.com/drive/folders/1XmKF0LOmbtJW1cxMH4cV64Cutxl30GjR"
         >
           Guidebook
         </a>
